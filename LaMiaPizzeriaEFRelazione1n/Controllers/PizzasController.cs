33c670bea8dca64060ce88fc5e27e6d8deb3c24f
lucianocecere07 <cecereluciano97@gmail.com>
2023-01-23 18:16:32 +0100

    Aggiunte Cards delle pizze in Home/index, axios richiama il metodo Get()

--- LaMiaPizzeriaEFRelazione1n/Controllers/PizzasController.cs
+++ LaMiaPizzeriaEFRelazione1n/Controllers/PizzasController.cs
@@ -2,6 +2,7 @@
 using LaMiaPizzeriaEFRelazione1n.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace LaMiaPizzeriaEFRelazione1n.Controllers
 {
@@ -14,7 +15,7 @@ namespace LaMiaPizzeriaEFRelazione1n.Controllers
         {
             using (PizzeriaContext db = new PizzeriaContext())
             {
-                List<Pizza> pizze = db.Pizza.ToList();
+                List<Pizza> pizze = db.Pizza.Include(pizza => pizza.Ingredienti).ToList();
                 return Ok(pizze);
             }
         }
