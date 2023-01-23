f2506c0abafc9c9475c2b8d1277b42f51520df1e
lucianocecere07 <cecereluciano97@gmail.com>
2023-01-23 15:46:13 +0100

    Aggiunto Controller PizzasController per WebApi

---
+++ LaMiaPizzeriaEFRelazione1n/Controllers/PizzasController.cs
@@ -0,0 +1,22 @@
+﻿using LaMiaPizzeriaEFRelazione1n.DataBase;
+using LaMiaPizzeriaEFRelazione1n.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LaMiaPizzeriaEFRelazione1n.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PizzasController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            using (PizzeriaContext db = new PizzeriaContext())
+            {
+                List<Pizza> pizze = db.Pizza.ToList();
+                return Ok(pizze);
+            }
+        }
+    }
+}
