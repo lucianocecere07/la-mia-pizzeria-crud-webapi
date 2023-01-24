9203c658aa78ec3840872848bcac82edc80410a8
lucianocecere07 <cecereluciano97@gmail.com>
2023-01-24 16:13:24 +0100

    Modifica Metodo per Ricerca della Pizza

--- LaMiaPizzeriaEFRelazione1n/Controllers/PizzasController.cs
+++ LaMiaPizzeriaEFRelazione1n/Controllers/PizzasController.cs
@@ -3,6 +3,7 @@ using LaMiaPizzeriaEFRelazione1n.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
 
 namespace LaMiaPizzeriaEFRelazione1n.Controllers
 {
@@ -11,11 +12,21 @@ namespace LaMiaPizzeriaEFRelazione1n.Controllers
     public class PizzasController : ControllerBase
     {
         [HttpGet]
-        public IActionResult Get()
+        public IActionResult Get(string? ricerca)
         {
             using (PizzeriaContext db = new PizzeriaContext())
             {
-                List<Pizza> pizze = db.Pizza.Include(pizza => pizza.Ingredienti).ToList();
+                List<Pizza> pizze = new List<Pizza>();
+
+                if (ricerca == null || ricerca == "")
+                {
+                    pizze = db.Pizza.Include(pizza => pizza.Ingredienti).ToList();
+                }
+                else
+                {
+                    pizze = db.Pizza.Where(pizza => pizza.Name.ToLower().Contains(ricerca)).Include(pizza => pizza.Ingredienti).ToList();
+                }
+
                 return Ok(pizze);
             }
         }
